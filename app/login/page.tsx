9cb2617497bc460f7440091aadc10e48df143a3e
JangHyunjeong <jang.hyunjeong01@gmail.com>
2024-01-05 18:07:22 +0900

    Feat:  Try 로티

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,8 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import LoginForm from '@/app/ui/login-form';
+import Lottie from 'lottie-react';
+// import {lottie} from '@/app/ui/img/complete.json'
+// https://velog.io/@bjy100/%EB%A6%AC%EC%95%A1%ED%8A%B8-%EB%A1%9C%ED%8B%B0-%EC%A0%81%EC%9A%A9%ED%95%98%EB%8A%94-%EB%B0%A9%EB%B2%95
 
 export default function LoginPage() {
   return (
@@ -8,6 +11,9 @@ export default function LoginPage() {
         <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
           <div className="w-32 text-white md:w-36">
             <AcmeLogo />
+          </div>
+          <div>
+            <p>로티</p>
           </div>
         </div>
         <LoginForm />
